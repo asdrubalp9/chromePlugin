944baaeb18bf4c25965b30b6c805f804676477d8
Asdrúbal Pérez <asdrubalp9@gmail.com>
2023-05-25 08:56:35 -0400

    second commit

--- content.js
+++ content.js
@@ -1,13 +1,7 @@
 //TODO:
 // agregar funcion para saber si termino de escribir chad
 // que al descargar el html, se copien todos los estilos y se agreguen
-
-// console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaa')
-// fetch('https://cdn.jsdelivr.net/npm/sweetalert2@11')
-//     .then(response => response.text())
-//     .then(text => eval(text))
-//     .catch(error => console.error('Error loading SweetAlert2:', error));
-
+// instalar SWAL
 
 
 function HTMLInjector(HTML, selector, position) {
@@ -41,17 +35,27 @@ HTMLInjector(MicBtnHTML, 'textarea','afterend');
 HTMLInjector(speakBtnHTML, 'main .flex-1 .group:nth-child(odd) .relative .text-gray-400 .flex.gap-1 button:last-child','afterend');
 
 
+const copyPasteBtn = `
+    <button id="copyPasteBtn" style="position: fixed;top: 6em;right: 1em;border-radius: 50%;height: 50px;width: 50px;display: flex;align-items: center;justify-content: center;" class="btn btn-neutral shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M392.8 1.2c-17-4.9-34.7 5-39.6 22l-128 448c-4.9 17 5 34.7 22 39.6s34.7-5 39.6-22l128-448c4.9-17-5-34.7-22-39.6zm80.6 120.1c-12.5 12.5-12.5 32.8 0 45.3L562.7 256l-89.4 89.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-112-112c-12.5-12.5-32.8-12.5-45.3 0zm-306.7 0c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l112 112c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256l89.4-89.4c12.5-12.5 12.5-32.8 0-45.3z"/></svg>
+    </button>
+`;
 const downloadBtnHTML = `
-    <button id="downloadBtn" style="position: fixed;top: 1em;right: 1em;border-radius: 50%;height: 50px;width: 50px;display: flex;align-items: center;justify-content: center;" class="btn btn-primary">
+    <button id="downloadBtn" style="position: fixed;top: 1em;right: 1em;border-radius: 50%;height: 50px;width: 50px;display: flex;align-items: center;justify-content: center;" class="btn btn-primary shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style=" height: 20px; fill: #ffffff;"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
     </button>
 `;
 // Añade el botón al DOM
 document.body.insertAdjacentHTML('beforeend', downloadBtnHTML);
+document.body.insertAdjacentHTML('beforeend', copyPasteBtn);
 // Conecta el botón con la función de descarga
 document.getElementById('downloadBtn').addEventListener('click', () => {
     // Encuentra el elemento usando el selector CSS 
     downloadHTMLContent()
+});
+document.getElementById('copyPasteBtn').addEventListener('click', () => {
+    // Encuentra el elemento usando el selector CSS 
+    clearPastableContent()
 });
 function downloadHTMLContent() {
     selector = '#__next main'
@@ -102,19 +106,44 @@ function downloadHTMLContent() {
   }
 }
 
+function clearPastableContent() {
+  //check if have permission to get access to clipboard
+  navigator.permissions.query({name: "clipboard-read"}).then(result => {
+    if (result.state == "granted" || result.state == "prompt") {
+      navigator.clipboard.readText()
+      .then(pastableContent => {
+        let textarea = document.querySelector('textarea')
+        if(pastableContent && textarea){
+          let text = pastableContent.replace(/[\r\n]+/g, ' ');
+          console.log('pastableContent', pastableContent, text)
+          textarea.value += text
+          textarea.style.height=264+'px'
+          textarea.focus()
+        }else{
+          console.log('could not find textArea')
+        }
+      })
+      .catch(err => {
+        console.error('Failed to read clipboard contents: ', err);
+      });
+    }
+  });
+  
+}
 function chatgptResponseState() {
   let buttonSelector = document.querySelector('textarea').parentNode.querySelector('button:not(#micBtn)')
-  console.log('buttonSelector')
-  let initialContent = '<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 mr-1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>'
-    // Guarda el estado inicial cuando el contenido del botón sea igual al especificado
-    let intervalId = setInterval(function() {
-        const button = document.querySelector(buttonSelector);
-        if (button && button.textContent === initialContent) {
-            // Se encontró el botón y su contenido es el esperado
-            clearInterval(intervalId);
-            monitorButtonChanges(button, initialContent );
-        }
-    }, 200); // Verifica cada 200ms
+  console.log('buttonSelector',buttonSelector)
+  return
+  // let initialContent = '<svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="h-4 w-4 mr-1" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="22" y1="2" x2="11" y2="13"></line><polygon points="22 2 15 22 11 13 2 9 22 2"></polygon></svg>'
+  //   // Guarda el estado inicial cuando el contenido del botón sea igual al especificado
+  //   let intervalId = setInterval(function() {
+  //       const button = document.querySelector(buttonSelector);
+  //       if (button && button.textContent === initialContent) {
+  //           // Se encontró el botón y su contenido es el esperado
+  //           clearInterval(intervalId);
+  //           monitorButtonChanges(button, initialContent );
+  //       }
+  //   }, 200); // Verifica cada 200ms
 }
 
 function monitorButtonChanges(button, initialContent ) {
